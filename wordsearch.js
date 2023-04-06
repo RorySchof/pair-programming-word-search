cdce881b28215a1c22f34d47a2db10476ae6aa85
RorySchof <roryschofield@gmail.com>
2023-04-05 20:52:27 -0400

    Submittingworking version

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,16 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalJoin = letters[0].map((col, i) => letters.map(row => row[i]).join(''));
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    for (let l of verticalJoin) {
+        if (l.includes(word)) return true;
+      }
+    
+      return false;
 }
 
 module.exports = wordSearch
